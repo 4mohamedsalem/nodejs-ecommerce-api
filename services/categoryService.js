31c327b6ae47381e7eec47513f8b5cf935a4510e
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-02 01:08:34 +0200

    Get All Categories

--- services/categoryService.js
+++ services/categoryService.js
@@ -3,15 +3,19 @@ const asyncHandler = require("express-async-handler");
 
 const CategoryModel = require("../models/categoryModel");
 
-exports.getCategories = (req, res) => {
-  // const name = req.body.name;
-  // console.log(req.body);
-  res.send();
-};
+// @desc    Get List of categories
+// @route   GET /api/v1/categories
+// @access  Public
+exports.getCategories = asyncHandler(async (req, res) => {
+  const categories = await CategoryModel.find({});
+  res.status(200).json({ results: categories.length, data: categories });
+});
 
+// @desc    Create category
+// @route   POST /api/v1/categories
+// @access  Private
 exports.createCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
-  //async await
   const category = await CategoryModel.create({ name, slug: slugify(name) });
   res.status(201).json({ data: category });
 });
