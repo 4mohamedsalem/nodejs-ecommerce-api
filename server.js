fc0a5028bd617b030c2244e274c71b00554bf836
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-06 17:53:07 +0200

    Refactor our Global Error Middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan")
 
 dotenv.config({ path: "config.env" })
 const ApiError = require("./utils/apiError")
+const globalError = require("./middlewares/errorMiddleware")
 const dbConnection = require("./config/database")
 const categoryRoute = require("./routes/categoryRoute")
 
@@ -25,24 +26,11 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/categories", categoryRoute)
 
 app.all("*", (req, res, next) => {
-  // Create error and send it to error handling middleware
-  // const err = new Error(`Can't find this route: ${req.originalUrl}`)
-  // next(err.message)
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400))
 })
 
 // Global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode | 500
-  err.status = err.status || "error"
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  })
-})
+app.use(globalError)
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
