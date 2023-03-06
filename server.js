40888ace4d80c955ef487b2f81526082b54ad5c6
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-06 17:46:32 +0200

    Making our Reusable Error Class

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv")
 const morgan = require("morgan")
 
 dotenv.config({ path: "config.env" })
+const ApiError = require("./utils/apiError")
 const dbConnection = require("./config/database")
 const categoryRoute = require("./routes/categoryRoute")
 
@@ -15,7 +16,7 @@ const app = express()
 // Middleware
 app.use(express.json())
 
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
   console.log(`mode: ${process.env.NODE_ENV}`)
 }
@@ -25,13 +26,22 @@ app.use("/api/v1/categories", categoryRoute)
 
 app.all("*", (req, res, next) => {
   // Create error and send it to error handling middleware
-  const err = new Error(`Can't find this route: ${req.originalUrl}`)
-  next(err.message)
+  // const err = new Error(`Can't find this route: ${req.originalUrl}`)
+  // next(err.message)
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400))
 })
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
-  res.status(500).json({ err })
+  err.statusCode = err.statusCode | 500
+  err.status = err.status || "error"
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 const PORT = process.env.PORT || 8000
