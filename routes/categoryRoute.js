ad625859478ce04143b7faf9f29d9ab28329671c
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-07 21:27:48 +0200

    Refactor Validation Layer

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,5 +1,7 @@
 const express = require("express")
-const { param, validationResult } = require("express-validator")
+const {
+  getCategoryValidator,
+} = require("../utils/validators/categoryValidator")
 
 const {
   getCategories,
@@ -14,19 +16,7 @@ const router = express.Router()
 router.route("/").get(getCategories).post(createCategory)
 router
   .route("/:id")
-  .get(
-    // 1- rules
-    param("id").isMongoId().withMessage("Invalid category id"),
-    // 2- middleware => catch errors from rules if exist
-    (req, res) => {
-      // Finds the validation errors in this request and wraps them in an object with handy functions
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-      }
-    },
-    getCategory
-  )
+  .get(getCategoryValidator, getCategory)
   .put(updateCategory)
   .delete(deleteCategory)
 
