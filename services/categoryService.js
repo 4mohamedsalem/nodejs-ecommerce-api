670051d42c3d594c4b29298a366e35ae2f667483
Mohamed Salem <4mohamedsalem@gmail.com>
2023-04-03 23:41:51 +0200

    Create Sub Category Model, Handler, Validation and Route

--- services/categoryService.js
+++ services/categoryService.js
@@ -32,7 +32,7 @@ exports.getCategory = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/categories
 // @access  Private
 exports.createCategory = asyncHandler(async (req, res) => {
-  const name = req.body.name
+  const { name } = req.body
   const category = await Category.create({ name, slug: slugify(name) })
   res.status(201).json({ data: category })
 })
