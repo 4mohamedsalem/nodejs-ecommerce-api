0dcc1acef3479fcd151c7a892bac0776a54c5c13
Mohamed Salem <4mohamedsalem@gmail.com>
2023-04-06 06:38:58 +0200

    Create Product Model, Service, Validation and Route

---
+++ models/productModel.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [3, "Too short product title"],
+      maxLength: [64, "Too long product title"],
+    },
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+      minLength: [20, "Too short product description"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Product quantity is required"],
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      max: [200000, "Too long product price"],
+    },
+    priceAfterDiscount: {
+      type: Number,
+    },
+    colors: [String],
+
+    imageCover: {
+      type: String,
+      required: [true, "Product Image cover is required"],
+    },
+    images: [String],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Product must be belong to category"],
+    },
+    subcategory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Subcategory",
+      },
+    ],
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+    },
+    ratingsAverage: {
+      type: Number,
+      min: [1, "Rating must be above or equal 1.0"],
+      max: [5, "Rating must be below or equal 5.0"],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("Product", productSchema)
---
+++ utils/validators/productValidator.js
@@ -0,0 +1,87 @@
+const { check } = require("express-validator")
+const validatorMiddleware = require("../../middlewares/validatorMiddleware")
+
+exports.createProductValidator = [
+  check("title")
+    .isLength({ min: 3 })
+    .withMessage("must be at least 3 characters")
+    .notEmpty()
+    .withMessage("Product required"),
+  check("description")
+    .isLength({ max: 2000 })
+    .withMessage("Too long description")
+    .notEmpty()
+    .withMessage("Product description is required"),
+  check("quantity")
+    .notEmpty()
+    .withMessage("Product quantity is required")
+    .isNumeric()
+    .withMessage("Product quantity must be a number"),
+  check("sold")
+    .optional()
+    .isNumeric()
+    .withMessage("Product quantity must be a number"),
+  check("price")
+    .notEmpty()
+    .withMessage("Product price is required")
+    .isNumeric()
+    .withMessage("Product quantity must be a number")
+    .isLength({ max: 200000 })
+    .withMessage("To long price"),
+  check("priceAfterDiscount")
+    .optional()
+    .isNumeric()
+    .withMessage("Product priceAfterDiscount must be a number")
+    .isFloat()
+    .custom((value, { req }) => {
+      if (req.body.price < value) {
+        throw new Error("priceAfterDiscount must be lower than price")
+      }
+      return true
+    }),
+  check("colors")
+    .optional()
+    .isArray()
+    .withMessage("color should be array of string"),
+  check("imageCover").notEmpty().withMessage("Product imageCover is required"),
+  check("images")
+    .optional()
+    .isArray()
+    .withMessage("color should be array of string"),
+  check("category")
+    .notEmpty()
+    .withMessage("Product must be belong to a category")
+    .isMongoId()
+    .withMessage("Invalid Id format"),
+  check("subcategory").optional().isMongoId().withMessage("Invalid Id format"),
+  check("brand").optional().isMongoId().withMessage("Invalid Id format"),
+  check("ratingsAverage")
+    .optional()
+    .isNumeric()
+    .withMessage("ratingsAverage must be a number")
+    .isLength({ min: 1 })
+    .withMessage("Rating must be above or equal 1.0")
+    .isLength({ max: 5 })
+    .withMessage("Rating must be above or equal 5.0"),
+  check("ratingsQuantity")
+    .optional()
+    .isNumeric()
+    .withMessage("ratingsQuantity must be a number"),
+
+  validatorMiddleware,
+]
+
+exports.getProductValidator = [
+  check("id").isMongoId().withMessage("Invalid Product ID format"),
+  validatorMiddleware,
+]
+
+exports.updateProductValidator = [
+  check("id").isMongoId().withMessage("Invalid Product ID format"),
+  validatorMiddleware,
+]
+
+exports.deleteProductValidator = [
+  check("id").isMongoId().withMessage("Invalid Product ID format"),
+  validatorMiddleware,
+]
